35b8872564907fc38d3984c3e6e5a890f71324e0
NFLD99 <NFLD99@users.noreply.github.com>
2022-03-25 17:06:45 -0230

    Updated

---
+++ syntax/readme.md
@@ -0,0 +1,44 @@
+<div id="top"></div>
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+    <li><a href="#installation">Installation</a></li>
+    <li><a href="#usage">Usage</a></li>
+    </li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE COMMAND -->
+## About The Command
+
+[![Output Screenshot][Output-screenshot]](https://i.imgur.com/ilXSrkL.png)
+
+This command helps with finding the syntax to various commands in rise.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+### Installation
+
+1. Download the .py file and playce it in your rise scrips folder
+2. Restart rise
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+Command:
+```
+[prefix]syntax [command]
+```
+Output: [using avatar as an example]
+[![Output Screenshot][Output-screenshot]](https://i.imgur.com/ilXSrkL.png)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
--- syntax/syntax.py
+++ syntax/syntax.py
@@ -6,15 +6,15 @@ async def syntax(ctx, cmd):
     try:
         data = urllib.request.urlopen("https://raw.githubusercontent.com/NFLD99/Rise-Commands/main/syntax/cmdSyntaxList.json").read()
         output = json.loads(data)
-            for i in output[cmd]:
-                i1= str(i)
-                i2= i1.replace("{", "")
-                i3= i2.replace("}", "")
-                i4= i3.replace("\', \'", "\'\n\'")
-                theSyntax= "```py\n" + cmd + ":\n" + i4 + "\n```"
-                await ctx.send(theSyntax, delete_after=15)
-                print(theSyntax)
-            # await ctx.send(syntaxFull)
+        for i in output[cmd]:
+            i1= str(i)
+            i2= i1.replace("{", "")
+            i3= i2.replace("}", "")
+            i4= i3.replace("\', \'", "\'\n\'")
+            theSyntax= "```py\n" + cmd + ":\n" + i4 + "\n```"
+            await ctx.send(theSyntax, delete_after=15)
+            print(theSyntax)
+        # await ctx.send(syntaxFull)
     except Exception as e:
         print(str(e))
         await ctx.send("Command Not Found!\nMake sure you use all lowercase", delete_after=15)
