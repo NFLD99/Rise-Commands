640017cda0fb5f1f4b173cc4cfc52cda98599537
NFLD99 <NFLD99@users.noreply.github.com>
2022-05-05 01:55:41 -0230

    updated for new discord classes

--- TamperMonkeyScripts/userscript.user.js
+++ TamperMonkeyScripts/userscript.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Get Your Token [alt + T] | Login with token [alt + L]
 // @namespace    https://nfld99.com/therabbithacks.io.html
-// @version      0.1
+// @version      0.2
 // @description  Copy Or Download Your token
 // @author       Mr.Fluff
 // @match        https://discord.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
 // @grant        none
-// @require     https://raw.githubusercontent.com/eligrey/FileSaver.js/master/dist/FileSaver.min.js
+// @require      https://raw.githubusercontent.com/eligrey/FileSaver.js/master/dist/FileSaver.min.js
 // ==/UserScript==
 
 // If User Wants To Download The Token, Download A .Txt With The Account Name And Token Within
@@ -20,7 +20,7 @@ function dl(txt, name) {
 function onAltt() {
     var tkn = (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()
     var cpy = confirm("Copy or Download Token? [ok = copy, Cancel = Download]")
-    var dcn = document.querySelector("#app-mount > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div.container-1eFtFS > div > div.content-1SgpWY > div.sidebar-1tnWFu.hasNotice-1s68so > section > div.container-YkUktl > div.nameTag-sc-gpq.canCopy-IgTwyV > div.colorStandard-21JIj7.size14-3fJ-ot.usernameContainer-3PPkWq > div").innerText
+    var dcn = document.querySelector('[class="text-sm-normal-3Zj3Iv title-338goq"]').innerText
 
 // if "Ok" Copy Token To Clipboard
     if (cpy == true){
